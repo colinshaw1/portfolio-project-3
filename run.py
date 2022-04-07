f195310a1a2e290f96fe5bb06b0ea6d80118bad8
Colin Shaw <colin.shaw400@outlook.com>
2022-04-07 21:06:50 +0000

    Add basic while look to cycle through basic of the game and see if it works without any functions

--- run.py
+++ run.py
@@ -35,4 +35,31 @@ SINKINGSHIP_PIC = ['''
 
 word = 'COLIN'
 secretWord = list(len(word)*'_')
-print(secretWord)
+
+
+lives = 6
+gameWon = False
+
+while gameWon == False and lives > 0:
+    print(secretWord)
+    guess = input('Please enter a letter:')
+    guess = guess.upper()
+
+    if guess == word:
+        gameWon = True
+    if len(guess) == 1 and guess in word:
+        for i in range(0, len(word)):
+          letter = word[i]
+          if guess == letter:
+            secretWord[i] = guess
+        if '_' not in secretWord:
+            gameWon = True
+
+
+    else:
+        lives -= 1
+
+if gameWon:
+    print("Yes the word was +word+! You are the winnner")
+else:
+    print("You lose the word was: "' + word + '"!")
