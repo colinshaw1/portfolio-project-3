7c3bb6311703ee258914a967bfe191c5c32ad7b9
Colin Shaw <colin.shaw400@outlook.com>
2022-04-07 18:43:41 +0000

    Add SINKINGSHIP pic to be printed to the terminal

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+SINKINGSHIP_PIC = ['''
+      ______        
+     /     /
+ ___/_____/___    
+ \           / 
+  \_________/ ''','''
+
+      ______        
+     /     /
+ ___/_____/___    
+ \           / 
+  \         /''','''
+
+      ______        
+     /     /
+ ___/_____/___    
+ \           /''',''' 
+ 
+     ______        
+     /     /
+ ___/_____/___''','''
+
+     ______        
+     /     /
+    /     /''','''
+   
+     ______        
+    /     /''','''
+
+     ______''']
