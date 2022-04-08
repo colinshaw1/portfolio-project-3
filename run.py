9be91349f57eacd4bae3681fa064292041feef10
Colin Shaw <colin.shaw400@outlook.com>
2022-04-08 20:27:33 +0000

    Add sinking ship images to display function

--- run.py
+++ run.py
@@ -56,6 +56,7 @@ def display():
     Funtcion to display ships and current status
     '''
     print(SINKINGSHIP_PIC[6-livesLeft])
+    print(f'You have {livesLeft} left')
     print(secretWord)
 
 while gameState == False and livesLeft > 0:
