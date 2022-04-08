6912101d1ff8eebd2faaab4e69c77cd74a4b7b20
Colin Shaw <colin.shaw400@outlook.com>
2022-04-08 06:59:44 +0000

    Fixing print statements for when the game is finished

--- run.py
+++ run.py
@@ -33,33 +33,31 @@ SINKINGSHIP_PIC = ['''
 # restrict rubbish fraud warrant hand image revoke execute cycle functional burst rehabilitation herb grandmother'
 
 
-word = 'COLIN'
-secretWord = list(len(word)*'_')
+wordSelection = 'COLIN'
+secretWord = list(len(wordSelection)*'_')
 
 
-lives = 6
-gameWon = False
+livesLeft = 6
+gameState = False
 
-while gameWon == False and lives > 0:
+while gameState == False and livesLeft > 0:
     print(secretWord)
-    guess = input('Please enter a letter:')
+    guess = input('Please enter a letter: ')
     guess = guess.upper()
 
-    if guess == word:
-        gameWon = True
-    if len(guess) == 1 and guess in word:
-        for i in range(0, len(word)):
-          letter = word[i]
+    if guess == wordSelection:
+        gameState = True
+    if len(guess) == 1 and guess in wordSelection:
+        for i in range(0, len(wordSelection)):
+          letter = wordSelection[i]
           if guess == letter:
-            secretWord[i] = guess
+            wordSelection[i] = guess
         if '_' not in secretWord:
-            gameWon = True
-
-
+            gameState = True
     else:
-        lives -= 1
+        livesLeft -= 1
 
-if gameWon:
-    print("Yes the word was +word+! You are the winnner")
+if gameState:
+    print(f"Yes the word was {wordSelection}! You are the winnner")
 else:
-    print("You lose the word was: "' + word + '"!")
+    print(f"You lose the word was: {wordSelection}!")
