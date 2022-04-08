4e366741dbadc61ac1772cbb530100a4d7fe0dbe
Colin Shaw <colin.shaw400@outlook.com>
2022-04-08 20:26:01 +0000

    Add sinking ship images to display function

--- run.py
+++ run.py
@@ -34,10 +34,8 @@ SINKINGSHIP_PIC = ['''
 
 
 wordSelection = 'COLIN'
-wordSelection = wordSelection.lower()
+wordSelection = wordSelection.upper()
 secretWord = list(len(wordSelection)*'_')
-
-
 livesLeft = 6
 gameState = False
 
@@ -47,11 +45,18 @@ def guess_letter(letter, wordSelection):
     for i in range(0, len(wordSelection)):
         letter = wordSelection[i]
         if guess == letter:
-            wordSelection[i] = guess
+            secretWord[i] = guess
     if '_' not in secretWord:
         return True
     else:
         return False
+
+def display():
+    '''
+    Funtcion to display ships and current status
+    '''
+    print(SINKINGSHIP_PIC[6-livesLeft])
+    print(secretWord)
 
 while gameState == False and livesLeft > 0:
     print(secretWord)
@@ -60,10 +65,12 @@ while gameState == False and livesLeft > 0:
 
     if guess == wordSelection:
         gameState = True
+        secretWord = wordSelection
     if len(guess) == 1 and guess in wordSelection:
         gameState = guess_letter(guess, wordSelection)        
     else:
         livesLeft -= 1
+    display()
 
 if gameState:
     print(f"Yes the word was {wordSelection}! You are the winnner")
