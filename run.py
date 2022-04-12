ba24aeddecce747df037651b731ea83dbad9c5be
Colin Shaw <colin.shaw400@outlook.com>
2022-04-12 18:01:00 +0000

    Update play again function

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-import os, random
+import os
+import random
 SINKINGSHIP_PIC = ['''
       ______
      /     /
@@ -30,7 +31,7 @@ SINKINGSHIP_PIC = ['''
 
      ______''']
 
-#words taken from Random word generator 50 in total
+# words taken from Random word generator 50 in total
 wordSelection = '''
 goalkeeper
 improve
@@ -95,7 +96,8 @@ def guess_letter(letter, wordSelection):
     '''
     This function checkt to see if a letter in a word from the word selection
     '''
-    global secretWord 
+    global secretWord
+    letter
     for i in range(0, len(wordSelection)):
         letter = wordSelection[i]
         if guess == letter:
@@ -105,22 +107,34 @@ def guess_letter(letter, wordSelection):
     else:
         return False
 
+
 def display():
     '''
     Funtcion to display ships and current status
     '''
     os.system("clear")
-    print(SINKINGSHIP_PIC[6-livesLeft])    
-    print(' '.join([str(e) for e in secretWord]))#converts to a string and The join() method takes all items in an iterable and joins them into one string.
+    print(SINKINGSHIP_PIC[6-livesLeft])
+    # converts to a string and The join() method takes all items in an iterable and joins them into one string.
+    print(' '.join([str(e) for e in secretWord]))
     print(f'You have {livesLeft} lives left')
+
 
 def playAgian():
     '''
     Function to return true of the player wants to play again
     if not return false
     '''
-    print("Would you like to play another game of SinkingShips?(yes or no)")
-    return input().upper.startswith('y')
+    # print("Would you like to play another game of SinkingShips?(yes or no)")
+    # return input().upper.startswith('y')
+    global playGame
+    playGame = input("Would you like to play another game of Sinking Ships? y = yes or n = no \n")
+    while playGame not in ["y", "Y", "n", "N"]:
+        playGame = input("Would you like to play another game of Sinking Ships? y = yes or n = no \n")
+    if playGame == "y":
+        secretWord = True
+    elif playGame == "n":
+        print("Thank you for playing Sinking Ships! See you soon")
+        exit()
 
 while gameState == False and livesLeft > 0:
     guess = input('Please enter a letter: ')
@@ -130,25 +144,27 @@ while gameState == False and livesLeft > 0:
         gameState = True
         secretWord = wordSelection
     if len(guess) == 1 and guess in wordSelection:
-        gameState = guess_letter(guess, wordSelection)        
+        gameState = guess_letter(guess, wordSelection)
     else:
         livesLeft -= 1
     display()
+    
    
+    # if gameState:
+    #     if playAgian():
+    #         wordSelection = random.choice(wordSelection)
+    #         wordSelection = wordSelection.upper()
+    #         secretWord = list(len(wordSelection)*'_')
+    #         gameState = False
+    #     else:
+    #         break
 
 if gameState:
     print(f"Yes the word was {wordSelection}! You are the winnner")
 else:
-    print(f"You lose this round of sinking ships, the word was: {wordSelection}!")
-    # playAgian() 
+    print(
+        f"You lose this round of sinking ships, the word was: {wordSelection}!")
+    playAgian()
 
-while True:
-    if gameState:
-        if playAgian():
-            wordSelection = random.choice(wordSelection)
-            wordSelection = wordSelection.upper()
-            secretWord = list(len(wordSelection)*'_')
-            livesLeft = 6
-            gameState = False
-        else:
-            break
+
+
